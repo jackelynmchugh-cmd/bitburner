2278a53a2b72f69ed166d0a92e72b2d147fd4826
jackelynmchugh-cmd <jackelyn.mchugh@gmail.com>
2026-02-11 21:50:09 -0700

    Add new programs to purchase list
    Added 'Formulas.exe' and 'DarkscapeNavigator.exe' to the list of programs to purchase.

--- darkweb-buyer.js
+++ darkweb-buyer.js
@@ -2,9 +2,11 @@
 export async function main(ns) {
   if (!ns.singularity?.purchaseProgram || !ns.singularity?.purchaseTor) return;
   if (!ns.getPlayer().tor) ns.singularity.purchaseTor();
-  if (!ns.getPlayer().tor) return;
 
-  const progs = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe", "ServerProfiler.exe", "DeepscanV1.exe", "DeepscanV2.exe", "AutoLink.exe"];
+  const progs = ["BruteSSH.exe", "FTPCrack.exe", "relaySMTP.exe", "HTTPWorm.exe", "SQLInject.exe",
+                 "ServerProfiler.exe", "DeepscanV1.exe", "DeepscanV2.exe", "AutoLink.exe",
+                 "Formulas.exe", "DarkscapeNavigator.exe"]; // added
+
   for (const p of progs) {
     if (!ns.fileExists(p, "home")) ns.singularity.purchaseProgram(p);
   }
